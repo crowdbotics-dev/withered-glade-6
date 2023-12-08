c572badfc5da649882aaf1a7cf062c65f3dfc5d4
Crowdbotics <engineering@crowdbotics.com>
2023-12-08 06:04:36 +0000

    Initial commit

---
+++ backend/withered_glade_6/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for withered_glade_6 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "withered_glade_6.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-glade-6",
+  url: "https://withered-glade-6.botics.co",
+  api: "https://withered-glade-6.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
